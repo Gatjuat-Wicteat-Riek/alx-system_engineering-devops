b49d2dac0cc2d8b30088b20cc9688b59be265c09
Gatjuat-Wicteat-Riek <gatjuatriek@gmail.com>
2024-05-09 13:16:01 -0700

    Advanced API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,16 @@
+#!/usr/bin/python3
+"""Function to query subscribers on a given Reddit subreddit."""
+import requests
+
+
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or not isinstance(subreddit, str):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/riek_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    try:
-        r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                         headers={'User-Agent': '0x16-api_advanced:project:v1.0.0 (by /u/colomriek)'}).json()
-        subs = r.get("data", {}).get("subscribers", 0)
-        return subs
-    except requests.RequestException as e:
-        print("Error fetching subreddit data:", e)
-        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
